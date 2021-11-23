4ed9226a4abf02030b733d4ec268e92b1a3cba2b
Frederik Bolding <frederik.bolding@gmail.com>
2021-11-23 17:18:33 +0100

    Add indexing of contract state

--- src/erc721.ts
+++ src/erc721.ts
@@ -1,16 +1,22 @@
 import { ERC721 } from "../generated/schema";
-import { Transfer } from "../generated/templates/ERC721/ERC721";
-
+import { Transfer, ERC721 as ERC721Contract } from "../generated/ERC721/ERC721";
 export function handleTransfer(event: Transfer): void {
   const tokenId = event.params.tokenId;
   const tokenAddress = event.address;
+  const contract = ERC721Contract.bind(tokenAddress);
   let id = `${tokenAddress.toHex()}_${tokenId}`;
   let token = ERC721.load(id);
   if (token == null) {
     token = new ERC721(id);
     token.tokenId = tokenId;
     token.address = tokenAddress;
+    const name = contract.try_name()
+    token.name = !name.reverted ? name.value : null;
+    const symbol = contract.try_symbol();
+    token.symbol = !symbol.reverted ? symbol.value : null;
   }
   token.owner = event.params.to;
+  const uri = contract.try_tokenURI(tokenId);
+  token.uri = !uri.reverted ? uri.value : null;
   token.save();
 }
